b966e7e98fd62ef20c7917b9f5066662b167f68a
RenanSaravalli <renansaravalli@gmail.com>
2025-11-01 16:56:13 -0300

    feat: Cria classes para Arquitetura por camadas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepositoy.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepositoy extends JpaRepository<MissaoModel, Long> {
+
+}
